3a76c7977f131de4f97ff641a05c39b2560fdc03
Mike Dilley <mike.dilley@gmail.com>
2018-11-01 14:32:16 +0000

    Test updated

--- lib/animal.rb
+++ lib/animal.rb
@@ -2,4 +2,16 @@ class Animal
 
   attr_accessor :name, :kingdom, :phylum, :klass, :order, :family, :genus, :species
 
+  @@all = []
+
+  #Hooks
+  def initialize
+
+  end
+
+  # self.all - Class method (Getter)
+  def self.all
+    @@all
+  end
+
 end
